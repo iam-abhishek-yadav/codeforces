3c44c1ad3c43cef478f69debed0f45091c905e6b
iam-abhishek-yadav <abhrecscs17@gmail.com>
2026-09-03 15:28:52 +0530

    Add C++ solutions for 800-rated problems.

---
+++ 800/1789A_Serval_And_Mocha's_Array.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        bool possible = false;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (gcd(a[i], a[j]) <= 2) {
+                    possible = true;
+                    break;
+                }
+            }
+
+            if (possible)
+                break;
+        }
+
+        cout << (possible ? "Yes" : "No") << '\n';
+    }
+
+    return 0;
+}
