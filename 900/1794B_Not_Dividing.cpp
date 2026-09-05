3933fb8c8064573bdd496fe079f84cf3cf18fdd5
iam-abhishek-yadav <abhrecscs17@gmail.com>
2026-09-05 13:16:28 +0530

    Add C++ solutions for 900-rated problems.

---
+++ 900/1794B_Not_Dividing.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+
+        for (int &x : a)
+            cin >> x;
+
+        if (a[0] == 1)
+            a[0] = 2;
+
+        for (int i = 1; i < n; ++i) {
+            if (a[i] == 1)
+                a[i] = 2;
+
+            if (a[i] % a[i - 1] == 0)
+                ++a[i];
+        }
+
+        for (int x : a)
+            cout << x << ' ';
+
+        cout << '\n';
+    }
+
+    return 0;
+}
